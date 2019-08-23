a4e610dac4e9ae459547b15a2375d368b73d7a94
LittleTwo-zhifu <liyou_723613@163.com>
2019-08-23 14:49:28 +0800

    Initial Commit

---
+++ pages/ucenter/index/url.js
@@ -0,0 +1,66 @@
+// pages/ucenter/index/url.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    shopLogo: 'https://dcdn.it120.cc/2019/08/08/83ad2cc3-d986-43ab-9f6f-7cd07f0ef7db.jpg',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ utils/city.js
@@ -0,0 +1,4 @@
+var cityData = [{ "name": "四川省", "id": 510000, "cityList": [{ "districtList": [{ "name": "锦江区", "pid": 510100, "id": 510104 }, { "name": "青羊区", "pid": 510100, "id": 510105 }, { "name": "双流区", "pid": 510100, "id": 510116 }, { "name": "温江区", "pid": 510100, "id": 510115 }], "name": "成都市", "pid": 510000, "id": 510100 }] }];
+module.exports = {
+  cityData: cityData
+}
